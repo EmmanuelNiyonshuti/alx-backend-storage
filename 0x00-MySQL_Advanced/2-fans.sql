edecc6e36fb0859fd7f918b2e6ffa3c947b9b71d
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-08-21 17:40:46 +0200

    sql scripts for task 1, 2 and 3

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,1 +1,10 @@
 -- ranks country origins of bands, ordered by the number of(no-unique) fans.
+
+SELECT
+    origin, SUM(fans) as nb_fans
+FROM
+    metal_bands
+GROUP BY
+    origin
+ORDER BY
+    nb_fans DESC LIMIT 9;
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- list all bands with Glam rock as their main style, ranked by their longevity.
+-- Lifespan is calculated as the difference between the year (2022) and the formed year.
+-- If the split year is present, use it; otherwise, use 2022 as the end year.
+
+SELECT 
+    band_name, COALESCE(split, 2022) - formed AS lifespan
+FROM
+    metal_bands
+WHERE
+    style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
