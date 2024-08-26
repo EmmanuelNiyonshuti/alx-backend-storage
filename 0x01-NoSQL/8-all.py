8449f9a7c57214c0bdd0e63a9c32197ae6aa2f26
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-08-26 23:03:42 +0200

    should not execute when imported

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,15 +5,16 @@ implements a function that lists all documents in a collection.
 """
 from pymongo import MongoClient
 
-
-def list_all(mongo_collection):
-    """
-    lists all documents in a collection.
-    Args:
-        mongo_collection - pymongo collection object.
-    Return:
-        a list of all documents in the collection or an empty list otherwise.
-    """
-    if mongo_collection.count_documents({}) == 0:
-        return []
-    return [doc for doc in mongo_collection.find()]
+if __name__ == "__main__":
+    def list_all(mongo_collection):
+        """
+        lists all documents in a collection.
+        Args:
+            mongo_collection - pymongo collection object.
+        Return:
+            a list of all documents in the collection or
+            an empty list otherwise.
+        """
+        if mongo_collection.count_documents({}) == 0:
+            return []
+        return [doc for doc in mongo_collection.find()]
